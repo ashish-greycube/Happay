d52bb95cf0b00fa179e18513e46578e430978e81
durve-greycube <zelam@greycube.in>
2024-07-12 09:40:09 +0530

    fix:remove validation for pm and bank account

--- happay/happay/doctype/vendor_invoice/vendor_invoice.js
+++ happay/happay/doctype/vendor_invoice/vendor_invoice.js
@@ -85,14 +85,6 @@ frappe.ui.form.on("Vendor Invoice", {
             }
         })
             
-    },
-    before_save(frm){
-        if (frm.doc.project_manager==undefined || frm.doc.project_manager ==""){
-            frappe.throw(__("Project manager is missing, please set project manager in parent cost center"))
-        }
-        if (frm.doc.supplier_bank_account==undefined || frm.doc.supplier_bank_account ==""){
-            frappe.throw(__("Please set bank account for supplier"))
-        }
     }
 });
 
